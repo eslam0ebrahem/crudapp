cef4c40a740120cd737b971c92d7611cd55842e0
islam <eslam0ebrahem0@gmail.com>
2022-10-12 23:20:33 +0200

    responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,16 +5,18 @@
             <h1>CRUD</h1>
         </div>
         <div class="row text-center">
-            <div class="col"> <a href="{{ url('/employee') }}" class="btn w-100  btn-success btn-sm" title="Add New Employe">
+            <div class="col-sm-12 col-md-4 mb-3 mb-md-0"> <a href="{{ url('/employee') }}"
+                    class="btn w-100  btn-success btn-sm" title="Add New Employe">
                     <i class="fa fa-plus" aria-hidden="true"></i> employees<br />{{ $employees }}
                 </a>
             </div>
-            <div class="col"> <a href="{{ url('/dependent') }}" class="btn w-100 btn-info btn-sm" title="Add New Employe">
+            <div class="col-sm-12 col-md-4 mb-3 mb-md-0"> <a href="{{ url('/dependent') }}" class="btn w-100 btn-info btn-sm"
+                    style="color:white" title="Add New Employe">
                     <i class="fa fa-plus" aria-hidden="true"></i>dependents <br />{{ $dependents }}
                 </a>
             </div>
-            <div class="col"> <a href="{{ url('/department') }}" class="btn w-100 btn-secondary btn-sm"
-                    title="Add New Employe">
+            <div class="col-sm-12 col-md-4 mb-3 mb-md-0"> <a href="{{ url('/department') }}"
+                    class="btn w-100 btn-secondary btn-sm" title="Add New Employe">
                     <i class="fa fa-plus" aria-hidden="true"></i>departments<br /> {{ $departments }}
                 </a>
             </div>
